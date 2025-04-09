041803d0441c5d13c10d92cb5a2b208a9541415c
oheneemmanuel <ohenekwakye24@gmail.com>
2025-04-08 17:50:32 -0800

    on site planning

--- week02/scripts/dom.js
+++ week02/scripts/dom.js
@@ -1,25 +1,37 @@
 const input = document.querySelector('#favchap');
 const button = document.querySelector('button');
-const list = document.querySelector('ul');
+const list = document.querySelector('#list');
 
-const li = document.createElement('ul');
 
-const deleteButton = document.createElement('button');
-
-li.textContent = input.value;
-
-deleteButton.textContent = '❌';
-li.append(deleteButton);
-li.append(li);
 
 button.addEventListener('click', function() {
-    if (input.value.trim() !== '') {const deleteButton = document.createElement('button')};
+    if (input.value.trim() !== '') {
+        const li = document.createElement('ul');
+
+        const deleteButton = document.createElement('button');
+
+        li.textContent = input.value;
+
+
+        deleteButton.textContent = "❌";
+        li.append(deleteButton);
+        list.append(li);
+
+        deleteButton.addEventListener("click", function() {
+            list.removeChild(li);
+            input.value = "";
+            input.focus();
+        });
+
+
+    };
+   
+
+    
+        
+        
+
 
 });
 
-deleteButton.addEventListener('click', function () {
-    list.removeChild(li);
-    input.focus();
-    input.value = '';
-    
-})
+
